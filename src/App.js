36215cbd5261c70e5fc4f5c3f8d6484f3519e790
Madhukumar <madhumadhukumar9848@gmail.com>
2024-03-10 17:39:12 +0530

    send

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Weatherapp from './Components/WeatherApp/Weatherapp';
+
+function App() {
+  return (
+    <div className="App">
+      <Weatherapp/>
+    </div>
+  );
+}
+
+export default App;
